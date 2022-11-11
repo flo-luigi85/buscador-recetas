13bfe3b2623b110227b54f5d9acf5b7891b313e3
flo-luigi85 <flo.lugi85@gmail.com>
2022-11-10 23:25:15 -0300

    iniciando recetas

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-const recetas = 'comida';
+function iniciarApp() {
+    
+    const selectCategorias = document.querySelector('#categorias');
+
+  obtenerCategorias();
+
+  function obtenerCategorias() {
+    const url = "https://www.themealdb.com/api/json/v1/1/categories.php";
+    fetch(url)
+      .then((respuesta) => respuesta.json())
+      .then((resultado) => mostrarCategorias(resultado.categories));
+  }
+
+  function mostrarCategorias(categorias = []){
+    categorias.forEach(categoria =>{
+        const option = document.createElement('OPTION');
+        option.value = categoria.strCategory
+        option.textContent = categoria.strCategory
+     
+        
+    })
+  }
+}
+
+document.addEventListener("DOMContentLoaded", iniciarApp);
